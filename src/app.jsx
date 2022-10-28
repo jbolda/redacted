9657f04f526000e5dea142f97ddc6d0507c88990
Jacob Bolda <me@jacobbolda.com>
2022-10-28 13:34:30 -0500

    add color picker to change window color

--- src/app.jsx
+++ src/app.jsx
@@ -3,18 +3,21 @@ import preactLogo from "./assets/preact.svg";
 import { WebviewWindow } from "@tauri-apps/api/window";
 import "./app.css";
 
+import { HexAlphaColorPicker } from "react-colorful";
+
 export function App() {
   const [windows, setWindows] = useState([]);
 
   const createWindow = () => {
     setWindows((prevState) => {
       console.log(prevState);
-      const newWindowMeta = { id: `w${Date.now()}`, color: "black" };
+      const newWindowMeta = { id: `w${Date.now()}`, color: "#000000" };
       const webview = new WebviewWindow(newWindowMeta.id, {
         url: "../colors.html",
         decorations: false,
         center: true,
         resizable: true,
+        transparent: true,
       });
 
       const nextState = prevState.concat([newWindowMeta]);
@@ -39,15 +42,42 @@ export function App() {
     });
   };
 
+  const setWindowColor = (windowId) => (color) => {
+    setWindows((prevState) => {
+      const nextState = [...prevState].map((window) => {
+        if (window.id === windowId) {
+          window.color = color;
+        }
+        return window;
+      });
+      const webview = new WebviewWindow(windowId);
+      webview.emit("window-color-change", { color });
+      console.log(nextState);
+
+      return nextState;
+    });
+  };
+
   return (
-    <div class="container">
-      <h1>Welcome to Redacted!</h1>
-      {windows.map((window) => (
-        <button id={window.id} key={window.id} onClick={removeWindow}>
-          {window.color}
-        </button>
-      ))}
-      <button onClick={createWindow}>create</button>
-    </div>
+    <>
+      <nav class="container" data-tauri-drag-region>
+        <h1>Welcome to Redacted!</h1>
+      </nav>
+      <section>
+        {windows.map((window) => (
+          <div style={{ backgroundColor: window.color }}>
+            <HexAlphaColorPicker
+              color={window.color}
+              onChange={setWindowColor(window.id)}
+            />
+            <button id={window.id} key={window.id} onClick={removeWindow}>
+              {window.color}
+            </button>
+          </div>
+        ))}
+        <button onClick={createWindow}>create</button>
+      </section>
+      <footer data-tauri-drag-region />
+    </>
   );
 }
